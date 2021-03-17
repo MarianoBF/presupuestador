2d8c9ce3d5c7d809afc284f33b92146453c9b816
MarianoBF <mariano.b.fernandez@gmail.com>
2021-03-17 15:23:53 -0300

    cors

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const mysql = require('mysql2/promise');
 const app = express();
 
 const corsOptions = {
-    origin: "https://marianobf.github.io/"
+    origin: "https://marianobf.github.io"
 };
 
 app.use(cors(corsOptions))
