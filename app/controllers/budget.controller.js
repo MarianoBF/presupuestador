2126df52fb3d67d56e5a5454a38b4687e174a600
Mariano <mariano.b.fernandez@gmail.com>
2022-08-08 23:17:23 -0300

    WIP: token check in budget

--- app/controllers/budget.controller.js
+++ app/controllers/budget.controller.js
@@ -1,98 +1,107 @@
 const db = require("../models");
 const Budget = db.budget;
 const Op = db.Sequelize.Op;
+const chequearToken = require("../middleware/auth");
 
 exports.create = (req, res) => {
-    if (!req.body.category) {
+    console.log(chequearToken(req.headers["x-access-token"]).resultado)
+  try {
+    if (
+      chequearToken(req.headers["x-access-token"]).resultado === "Autorizado"
+    ) {
+      if (!req.body.category) {
         res.status(400).send({
-            message: "Falta rubro"
+          message: "Falta rubro",
         });
-        return
-    }
-    if (!req.body.description) {
+        return;
+      }
+      if (!req.body.description) {
         res.status(400).send({
-            message: "Falta descripción"
+          message: "Falta descripción",
         });
-        return
-    }
-    if (!req.body.limit) {
+        return;
+      }
+      if (!req.body.limit) {
         res.status(400).send({
-            message: "Falta monto"
+          message: "Falta monto",
         });
-        return
-    }
+        return;
+      }
 
-    const budget = {
+      const budget = {
         category: req.body.category,
         description: req.body.description,
-        limit: req.body.limit
-    }
+        limit: req.body.limit,
+      };
 
-    Budget.create(budget)
-        .then(data => {
-            res.send(data);
+      Budget.create(budget)
+        .then((data) => {
+          res.send(data);
         })
-        .catch(err => {
-            res.status(500).send({
-                message:
-                    err.message || "Ocurrió un error"
-            })
-        })
+        .catch((err) => {
+          res.status(500).send({
+            message: err.message || "Ocurrió un error",
+          });
+        });
+    } else {
+      res.status(401).send("Token inválido");
+    }
+  } catch {
+    res.status(400).send("Hubo un problema, revise los datos y reintente");
+  }
 };
 
 exports.findAll = (req, res) => {
-    const title = req.query.title;
-    let condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
+  const title = req.query.title;
+  let condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
 
-    Budget.findAll({ where: condition })
-        .then(data => {
-            res.send(data)
-        })
-        .catch(err => {
-            res.status(500).send({
-                message:
-                err.message || "Error al buscar."
-            })
-        })
+  Budget.findAll({ where: condition })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Error al buscar.",
+      });
+    });
 };
 
 exports.delete = (req, res) => {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    Budget.destroy({
-        where: { id: id }
+  Budget.destroy({
+    where: { id: id },
+  })
+    .then((data) => {
+      console.log(data);
+      if (+data === 1) {
+        res.status(204).send({
+          message: "Borrado con éxito",
+        });
+      } else {
+        res.status(400).send({
+          message: "No se pudo borrar id: " + id,
+        });
+      }
     })
-        .then(data=> {
-            console.log(data)
-            if (+data === 1) {
-                res.status(204).send({
-                    message: "Borrado con éxito"
-                });
-            } else {
-                res.status(400).send({
-                    message: "No se pudo borrar id: " +id
-                });
-            }
-            })
-            .catch(()=> {
-                res.status(500).send({
-                    message: "No se logró borrar id: " +id
-                });
-            });
-        };
+    .catch(() => {
+      res.status(500).send({
+        message: "No se logró borrar id: " + id,
+      });
+    });
+};
 
 exports.deleteAll = (req, res) => {
-    Budget.destroy({
-        where: {},
-        truncate: false
+  Budget.destroy({
+    where: {},
+    truncate: false,
+  })
+    .then((nums) => {
+      res.status(202).send({ message: `Todos (${nums}) los items borrados` });
     })
-    .then(nums=> {
-        res.status(202).send({ message: `Todos (${nums}) los items borrados`});
-    })
-    .catch(err=> {
-        res.status(500).send({
-            message:
-                err.message || "Hubo un problema al querer borrar todo"
-        });
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Hubo un problema al querer borrar todo",
+      });
     });
 };
